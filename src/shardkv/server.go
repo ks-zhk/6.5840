20987d7ed93c2d530be98c657a70b8d005fd2d91
alyjay <3110481@qq.com>
2023-06-30 12:29:30 +0800

    add some support for

--- src/shardkv/server.go
+++ src/shardkv/server.go
@@ -1,19 +1,89 @@
 package shardkv
 
-
-import "6.5840/labrpc"
+import (
+	"6.5840/labrpc"
+	"6.5840/utils"
+	"bytes"
+	"log"
+	"sync/atomic"
+)
 import "6.5840/raft"
 import "sync"
 import "6.5840/labgob"
 
+type ClientId struct {
+	ClerkId       int
+	NextCallIndex int
+}
 
+type CacheTable struct {
+	dupTable map[ClientId]*OpChan
+	IndexId  map[int]ClientId
+}
+
+func (ct *CacheTable) insert(clientId ClientId, op *OpChan) {
+	ct.dupTable[clientId] = op
+	ct.IndexId[op.index] = clientId
+}
+func (ct *CacheTable) tryGetOpChan(id ClientId) (*OpChan, bool) {
+	res, ok := ct.dupTable[id]
+	return res, ok
+}
+func (ct *CacheTable) tryGetByIndex(idx int) (*OpChan, bool) {
+	if id, ok := ct.IndexId[idx]; ok {
+		if op, okk := ct.dupTable[id]; okk {
+			return op, true
+		}
+	}
+	return nil, false
+}
+func (ct *CacheTable) clearByClientId(id ClientId) {
+	//delete(ct.dupTable, id)
+	if res, ok := ct.dupTable[id]; ok {
+		index := res.index
+		delete(ct.IndexId, index)
+		delete(ct.dupTable, id)
+	}
+}
+func (ct *CacheTable) clearByPreviousId(id ClientId) {
+	pid := id.previousCallIndex()
+	ct.clearByClientId(pid)
+}
+func (cd *ClientId) previousCallIndex() ClientId {
+	return ClientId{ClerkId: cd.ClerkId, NextCallIndex: cd.NextCallIndex - 1}
+}
+
+type OpRes int
+
+const (
+	OpResOk   OpRes = 0
+	OpUnknown OpRes = 1
+)
+
+type OpType int
+
+const (
+	AppendOp OpType = 0
+	PutOp    OpType = 1
+	GetLog   OpType = 2
+	NoneOp   OpType = 3
+)
 
 type Op struct {
 	// Your definitions here.
 	// Field names must start with capital letters,
 	// otherwise RPC will break.
+	Key  string
+	Args string
+	Type OpType
+	Cid  ClientId
 }
-
+type OpChan struct {
+	index int
+	cond  *utils.MCond
+	opRes OpRes
+	val   string
+}
 type ShardKV struct {
 	mu           sync.Mutex
 	me           int
@@ -25,9 +95,120 @@ type ShardKV struct {
 	maxraftstate int // snapshot if log grows this big
 
 	// Your definitions here.
+	dead                int32
+	clientQueryResStore map[ClientId]string
+	clientLastCallIndex map[int]int
+	lastApplied         int
+	cacheTable          CacheTable
+	snapshot            []byte
+	snapshotLastIndex   int
+	persister           *raft.Persister
+
+	stateMachine map[string]string
 }
 
-
+func (kv *ShardKV) applyCommand(op Op) string {
+	if op.Type == AppendOp {
+		if m, ok := kv.stateMachine[op.Key]; ok {
+			m += op.Args
+			kv.stateMachine[op.Key] = m
+		} else {
+			kv.stateMachine[op.Key] = op.Args
+		}
+	}
+	if op.Type == PutOp {
+		kv.stateMachine[op.Key] = op.Args
+	}
+	if res, ok := kv.stateMachine[op.Key]; ok {
+		return res
+	} else {
+		return ""
+	}
+}
+func (opChan *OpChan) ok(val string) {
+	opChan.cond.L.Lock()
+	opChan.opRes = OpResOk
+	opChan.val = val
+	opChan.cond.Broadcast()
+	opChan.cond.L.Unlock()
+}
+func (kv *ShardKV) applier(ch chan raft.ApplyMsg) {
+	for msg := range ch {
+		if kv.killed() {
+			return
+		}
+		if msg.CommandValid {
+			kv.mu.Lock()
+			if msg.CommandIndex != kv.lastApplied+1 {
+				kv.mu.Unlock()
+				continue
+			}
+			kv.lastApplied = msg.CommandIndex
+			op := msg.Command.(Op)
+			opChan, ok := kv.cacheTable.tryGetOpChan(op.Cid)
+			if _, okk := kv.clientLastCallIndex[op.Cid.ClerkId]; okk && op.Cid.NextCallIndex <= kv.clientLastCallIndex[op.Cid.ClerkId] {
+				if ok {
+					opChan.ok(kv.clientQueryResStore[op.Cid])
+				}
+				kv.mu.Unlock()
+				continue
+			}
+			val := kv.applyCommand(op)
+			if ok {
+				opChan.ok(val)
+			}
+			if op.Type == GetLog {
+				kv.clientQueryResStore[op.Cid] = val
+			}
+			kv.clientLastCallIndex[op.Cid.ClerkId] = op.Cid.NextCallIndex
+			if kv.maxraftstate != -1 && kv.persister.RaftStateSize() >= kv.maxraftstate {
+				kv.rf.Snapshot(kv.lastApplied, kv.makeSnapshotNoneLock(kv.lastApplied))
+			}
+			kv.mu.Unlock()
+		} else if msg.SnapshotValid {
+			if msg.Snapshot == nil {
+				panic("snapshot can not be nil")
+			}
+			kv.mu.Lock()
+			kv.installSnapshotNoneLock(msg.Snapshot)
+			kv.mu.Unlock()
+		} else {
+			panic("invalid apply Type")
+		}
+	}
+}
+func (kv *ShardKV) makeSnapshotNoneLock(snapshotIndex int) []byte {
+	w := new(bytes.Buffer)
+	e := labgob.NewEncoder(w)
+	e.Encode(snapshotIndex)
+	e.Encode(kv.clientLastCallIndex)
+	e.Encode(kv.stateMachine)
+	kv.lastApplied = snapshotIndex
+	return w.Bytes()
+}
+func (kv *ShardKV) installSnapshotNoneLock(snapshot []byte) {
+	if len(snapshot) == 0 {
+		return
+	}
+	r := bytes.NewBuffer(snapshot)
+	d := labgob.NewDecoder(r)
+	var lastIncludedIndex int
+	var clientApplyInfo map[int]int
+	var stateMachine map[string]string
+	if d.Decode(&lastIncludedIndex) != nil ||
+		d.Decode(&clientApplyInfo) != nil ||
+		d.Decode(&stateMachine) != nil {
+		log.Fatalf("snapshot decode error")
+	}
+	if lastIncludedIndex < kv.lastApplied {
+		return
+	}
+	//DPrintf("[s %v] install snapshot lastInclude Index = %v, clientApplyInfo = %v, stateMachine = %v\n", kv.me, lastIncludedIndex, clientApplyInfo, stateMachine)
+	kv.clientLastCallIndex = clientApplyInfo
+	kv.snapshotLastIndex = lastIncludedIndex
+	kv.lastApplied = lastIncludedIndex
+	kv.stateMachine = stateMachine
+}
 func (kv *ShardKV) Get(args *GetArgs, reply *GetReply) {
 	// Your code here.
 }
@@ -41,10 +222,14 @@ func (kv *ShardKV) PutAppend(args *PutAppendArgs, reply *PutAppendReply) {
 // in Kill(), but it might be convenient to (for example)
 // turn off debug output from this instance.
 func (kv *ShardKV) Kill() {
+	atomic.StoreInt32(&kv.dead, 1)
 	kv.rf.Kill()
 	// Your code here, if desired.
 }
-
+func (kv *ShardKV) killed() bool {
+	z := atomic.LoadInt32(&kv.dead)
+	return z == 1
+}
 
 // servers[] contains the ports of the servers in this group.
 //
@@ -91,7 +276,6 @@ func StartServer(servers []*labrpc.ClientEnd, me int, persister *raft.Persister,
 
 	kv.applyCh = make(chan raft.ApplyMsg)
 	kv.rf = raft.Make(servers, me, persister, kv.applyCh)
-
 
 	return kv
 }
